006b145e5c19a6d95e53ade4e1687cb94fb22871
Tucanu <rockerulmaniac454@gmail.com>
2022-03-28 08:48:49 +0300

    -Updated to 1.18.1 due to lack of support for 1.17.1 -Implementation for Omnitrixes coming soon

--- src/main/java/com/tucanu/tucanu_bentenmod/item/ModItems.java
+++ src/main/java/com/tucanu/tucanu_bentenmod/item/ModItems.java
@@ -1,12 +1,11 @@
 package com.tucanu.tucanu_bentenmod.item;
 
 import com.tucanu.tucanu_bentenmod.Tucanu_BenTenMod;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems
 {
